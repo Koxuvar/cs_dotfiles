5a7c44f8a3f21cd58bf089f13a1db319882bff21
Koxuvar <connor@mmvdesigns.com>
2024-12-21 21:33:55 -0600

    adding mason to nvim

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -23,6 +23,8 @@ return {
     },
     { "mbbill/undotree" },
     { "tpope/vim-fugitive" },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim"},
     { "neovim/nvim-lspconfig" },
     { "hrsh7th/cmp-nvim-lsp" },
     { "hrsh7th/nvim-cmp" },
