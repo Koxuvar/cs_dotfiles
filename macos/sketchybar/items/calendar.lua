846e71a7c36d0aa3390ed4ae80400723f0e22795
csmac <connor@somidwest.com>
2024-12-25 20:16:09 -0600

    update to sketchybar on mac

--- macos/sketchybar/items/calendar.lua
+++ macos/sketchybar/items/calendar.lua
@@ -1,18 +1,7 @@
 local settings = require("settings")
 local colors = require("colors")
 
--- Padding item required because of bracket
-sbar.add("item", { position = "right", width = settings.group_paddings })
-
-local cal = sbar.add("item", {
-    icon = {
-        color = colors.white,
-        padding_left = 12,
-        font = {
-            style = settings.font.style_map["Bold"],
-            size = 14.0,
-        },
-    },
+local time = sbar.add("item", {
     label = {
         color = colors.white,
         padding_right = 16,
@@ -31,17 +20,32 @@ local cal = sbar.add("item", {
     },
 })
 
--- Double border for calendar using a single item bracket
-sbar.add("bracket", { cal.name }, {
+local cal = sbar.add("item", {
+    icon = {
+        color = colors.white,
+        padding_left = 12,
+        font = {
+            style = settings.font.style_map["Bold"],
+            size = 14.0,
+        },
+    },
+    position = "right",
+    update_freq = 30,
+    padding_left = 1,
+    padding_right = 6,
     background = {
-        color = colors.transparent,
-        border_color = colors.grey,
-    }
+        color = colors.bg2,
+        border_color = colors.black,
+        border_width = 3,
+    },
 })
 
--- Padding item required because of bracket
-sbar.add("item", { position = "right", width = settings.group_paddings })
 
 cal:subscribe({ "forced", "routine", "system_woke" }, function(env)
-    cal:set({ icon = os.date("􀉉  %b %d"), label = os.date("\n 􀐫 %I:%M %p") })
+    cal:set({ icon = os.date("%b %d") })
+end)
+
+
+time:subscribe({ "forced", "routine", "system_woke" }, function(env)
+    time:set({ label = os.date("%I:%M %p") })
 end)
