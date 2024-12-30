790e3dbc916901a6058b3c6845609e51cb098b9a
Koxuvar <connor@mmvdesigns.com>
2024-12-29 23:30:59 -0600

    fix: adding brackets to multi plugin files

--- .config/nvim/lua/plugins/mason-lsp.lua
+++ .config/nvim/lua/plugins/mason-lsp.lua
@@ -1,6 +1,7 @@
-return
+return{
     { "williamboman/mason.nvim" },
     { "williamboman/mason-lspconfig.nvim" },
     { "neovim/nvim-lspconfig" },
     { "hrsh7th/cmp-nvim-lsp" },
     { "hrsh7th/nvim-cmp" }
+}
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,12 +1,14 @@
 return {
-            "nvim-telescope/telescope.nvim",
-            tag = "0.1.8",
-            dependencies = {
-                "nvim-lua/plenary.nvim",
-                {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'},
-            },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'},
         },
-        {
-            "nvim-telescope/telescope-frecency.nvim",
-            config = function() require("telescope").load_extension "frecency" end,
-        }
+    },
+    {
+        "nvim-telescope/telescope-frecency.nvim",
+        config = function() require("telescope").load_extension "frecency" end,
+    }
+}
